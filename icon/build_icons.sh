d58b4d903b29bd5f160cbbb09571f5d89633599d
Julius Hardt <info@julius-hardt.eu>
2024-08-18 06:19:54 +0200

    Add lage icon for Microsoft Edge Add-on submission

--- icon/build_icons.sh
+++ icon/build_icons.sh
@@ -3,7 +3,7 @@
 # Requires rsvg-convert
 # sudo apt install librsvg2-bin
 
-for size in 16 32 48 128
+for size in 16 32 48 128 300
 do
     echo "Building ${size}x${size}"
     rsvg-convert --width=$size --height=$size --keep-aspect-ratio icon.svg > icon$size.png
